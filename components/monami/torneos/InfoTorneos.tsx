d026af7f069bc3771ccfe6c0072c192ebab86aff
caterina <monicakowal13@gmail.com>
2025-05-14 17:42:46 -0300

    tiempos

---
+++ components/monami/torneos/InfoTorneos.tsx
@@ -0,0 +1,17 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+
+export default function InfoTorneos() {
+  return (
+    <Card>
+        <CardHeader>
+            <CardTitle>
+                   Finde 10/5/2025
+            </CardTitle>
+        </CardHeader>
+        <CardContent>
+
+        </CardContent>
+    </Card>
+  )
+}
