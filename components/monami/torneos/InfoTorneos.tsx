4600e4798d5c967f22ac493951d2e578323ac2a4
caterina <monicakowal13@gmail.com>
2025-05-14 17:49:54 -0300

    tiempos2

--- components/monami/torneos/InfoTorneos.tsx
+++ components/monami/torneos/InfoTorneos.tsx
@@ -1,17 +1,19 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 
 export default function InfoTorneos() {
   return (
     <Card>
-        <CardHeader>
-            <CardTitle>
-                   Finde 10/5/2025
-            </CardTitle>
-        </CardHeader>
-        <CardContent>
-
-        </CardContent>
+      <CardHeader>
+        <CardTitle>Cuarta Fecha</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p>
+          CUARTA FECHA de la ACS en MONAMI SQUASH DEL 13 AL 19 DE MAYO. Cierre
+          de inscrpciones Sábado 11 de mayo. No te quedes afuera! Sorteamos
+          entre los jugadores una estadia en Villa gral Belgrano!! INSCRIPCIONES
+          POR RANKEDIN o al 3512521739.
+        </p>
+      </CardContent>
     </Card>
-  )
+  );
 }
